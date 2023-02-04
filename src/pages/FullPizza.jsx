3b3defa8f70072c994ad3cb8464dff2172362589
Said Martazanov <martazanov2020@mail.ru>
2023-02-04 17:22:33 +0300

    update

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -7,11 +7,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 const FullPizza: React.FC = () => {
     const {id} = useParams()
     const navigate = useNavigate()
-    const [pizza, setPizza] = useState<{
-        imageUrl: string;
-        title: string;
-        price: number; 
-    }>()
+    const [pizza, setPizza] = useState()
 
    useEffect(() => {
      const fetchPizza = async () =>{
@@ -27,7 +23,7 @@ const FullPizza: React.FC = () => {
      fetchPizza()
    }, [])
    if(!pizza){
-    return <>"Загрузка ...."</>
+    return <div>"Загрузка ...."</div>
    }
     return (
         <div className='container'>
