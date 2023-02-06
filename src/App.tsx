7a5d2d668bcfd811116d61fe45336ad969d4007e
Said Martazanov <martazanov2020@mail.ru>
2023-02-06 19:13:22 +0300

    update no comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,23 @@
-
 import { Route, Routes } from 'react-router-dom';
-
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import "./scss/app.scss"
+import './scss/app.scss';
 import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
 // Надо очистить App от лишнего мусора
 
-
 function App() {
-
-
   return (
     <div className="App">
-
-   
-       <Routes>
-          <Route path='/' element={<MainLayout />}>
-              <Route path='' element={<Home/>} />
-              <Route path='cart' element={<Cart />} />
-              <Route path='pizza/:id' element={<FullPizza />} />
-          </Route>
-       </Routes>
-         
-            {/* <Routes>
-              
-            </Routes> */}
-        
-     
-      </div>
-
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="pizza/:id" element={<FullPizza />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,27 +1,26 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectSort, setSort } from "../store/slices/filterSlice";
-
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSort, setSort } from '../store/slices/filterSlice';
 
 type SortItem = {
   name: string;
   sortProperty: string;
-}
-
+};
 
 export const sortList: SortItem[] = [
-  { name: "популярности (DESC)", sortProperty: "raiting" },
-  { name: "популярности (ASK)", sortProperty: "-raiting" },
-  { name: "цене (DESC)", sortProperty: "price" },
-  { name: "цене (ASK)", sortProperty: "-price" },
-  { name: "алфавиту (DESC)", sortProperty: "title" },
-  { name: "алфавиту (ASK)", sortProperty: "-title" },
+  { name: 'популярности (DESC)', sortProperty: 'raiting' },
+  { name: 'популярности (ASK)', sortProperty: '-raiting' },
+  { name: 'цене (DESC)', sortProperty: 'price' },
+  { name: 'цене (ASK)', sortProperty: '-price' },
+  { name: 'алфавиту (DESC)', sortProperty: 'title' },
+  { name: 'алфавиту (ASK)', sortProperty: '-title' },
 ];
 
 const Sort: React.FC = () => {
   const dispatch = useDispatch();
   const sort = useSelector(selectSort);
-  const sortRef = useRef();
+  const sortRef = useRef<HTMLDivElement>(null);
+
   const [sortOpen, setSortOpen] = useState(false);
 
   const onClickSortItem = (obj: SortItem) => {
@@ -29,21 +28,18 @@ const Sort: React.FC = () => {
     setSortOpen(false);
   };
 
- useEffect(() => {
-
-    const handleClickOutside = (event)=>{
-      
-      const path = event.path || (event.composedPath && event.composedPath())
-      if(!path.includes(sortRef.current)){
-        setSortOpen(false)
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      const path = event.path || (event.composedPath && event.composedPath());
+      if (!path.includes(sortRef.current)) {
+        setSortOpen(false);
       }
-    }
-    document.body.addEventListener("click", handleClickOutside)
-   return () => {
-     document.body.removeEventListener("click", handleClickOutside )
-   }
- }, [])
- 
+    };
+    document.body.addEventListener('click', handleClickOutside);
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div ref={sortRef} className="sort">
@@ -53,8 +49,7 @@ const Sort: React.FC = () => {
           height="6"
           viewBox="0 0 10 6"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
@@ -71,10 +66,7 @@ const Sort: React.FC = () => {
                 <li
                   key={i}
                   onClick={() => onClickSortItem(obj)}
-                  className={
-                    sort.sortProperty === obj.sortProperty ? "active" : ""
-                  }
-                >
+                  className={sort.sortProperty === obj.sortProperty ? 'active' : ''}>
                   {obj.name}
                 </li>
               );
